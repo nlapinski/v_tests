57bc09596b9922bcd49f7f69b01baf312f24a147
Nate Lapinski <nate.lapinski@gmail.com>
2021-10-10 10:07:38 -0700

    cmake setup

---
+++ main.c
@@ -0,0 +1,17 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+  printf("main init");
+}
