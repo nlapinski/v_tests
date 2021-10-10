38233b775f7bd93cd250d6a082e8341dd23a7686
Nate Lapinski <nate.lapinski@gmail.com>
2021-10-10 10:40:14 -0700

    added submodules

--- main.c
+++ main.c
@@ -4,14 +4,75 @@
 #include <unistd.h>
 #endif
 
+#include <stdio.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+GLFWwindow* window;
+
+VkInstance instance;
+
+void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+}
+void createInstance() {
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
 
 
-
+    if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
+        printf("failed to create instance!\n");
+    }
+    else{
+        printf("started vulkan instance!\n");
+    }
+}
+void initVulkan() {
+    createInstance();
+}
+void cleanup() {
+    printf("shutting down\n");
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
 
 int main(int argc, char* argv[])
 {
 
-  printf("main init");
+
+  initWindow();
+  initVulkan();
+  
+  printf("main init \n");
+  while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+  }
+  cleanup();
+
 }
